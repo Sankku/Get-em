829da64b3896000cfdf383031cf9b5eecd36f915
Santiago Alaniz <santiagoemprendedor9@gmail.com>
2023-07-17 17:43:04 -0300

    skins & move functions

--- index.js
+++ index.js
@@ -6,38 +6,129 @@ canvas.height = innerHeight;
 
 class Player {
   constructor() {
-    this.position = {
-      x: 200,
-      y: 200,
-    };
     this.velocity = {
       x: 0,
       y: 0,
     };
 
     const image = new Image();
-    image.src = "./img/spaceship.png";
+    image.src = "./img/skin-1.png";
+    image.onload = () => {
+      const scale = 0.2;
+      this.image = image;
+      this.width = image.width * scale;
+      this.height = image.height * scale;
 
-    this.image = image;
-    this.width = 100;
-    this.height = 100;
+      this.position = {
+        x: canvas.width / 2 - this.width / 2,
+        y: canvas.height - this.height - 1,
+      };
+    };
   }
 
   draw() {
     // c.fillStyle = "red";
     // c.fillRect(this.position.x, this.position.y, this.width, this.height);
-    c.drawImage(this.image, this.position.x, this.position.y);
+
+    c.save();
+    c.translate(
+      player.position.x + player.width / 2,
+      player.position.y + player.height / 2
+    );
+    c.rotate(this.rotation);
+
+    c.translate(
+      -player.position.x - player.width / 2,
+      -player.position.y - player.height / 2
+    );
+
+    c.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+
+    c.restore();
+  }
+
+  update() {
+    if (this.image) {
+      this.draw();
+      this.position.x += this.velocity.x;
+    }
   }
 }
 
 const player = new Player();
-player.draw();
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  space: {
+    pressed: false,
+  },
+};
+
+this.rotation = 0;
 
 function animate() {
   requestAnimationFrame(animate);
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
-  player.draw();
+  player.update();
+
+  if (keys.a.pressed && player.position.x >= 0) {
+    player.velocity.x = -6;
+    player.rotation = -0.15;
+  } else if (
+    keys.d.pressed &&
+    player.position.x + player.width <= canvas.width
+  ) {
+    player.velocity.x = 6;
+    player.rotation = 0.15;
+  } else {
+    player.velocity.x = 0;
+    player.rotation = 0;
+  }
 }
 
 animate();
+
+addEventListener("keydown", ({ key }) => {
+  switch (key) {
+    case "a":
+      keys.a.pressed = true;
+      break;
+    case "d":
+      keys.d.pressed = true;
+      break;
+    case "s":
+      console.log("down");
+      break;
+    case "w":
+      console.log("up");
+      break;
+  }
+});
+
+addEventListener("keyup", ({ key }) => {
+  switch (key) {
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "s":
+      console.log("down");
+      break;
+    case "w":
+      console.log("up");
+      break;
+  }
+});
