8ba883496b01a42e83fb538217323d27b849129c
Santiago Alaniz <santiagoemprendedor9@gmail.com>
2023-07-19 15:30:41 -0300

    Add invaders movement by using grid

--- index.js
+++ index.js
@@ -61,7 +61,121 @@ class Player {
   }
 }
 
+class Invader {
+  constructor({ position }) {
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+
+    const image = new Image();
+    image.src = "./img/v-caster.png";
+    image.onload = () => {
+      const scale = 0.14;
+      this.image = image;
+      this.width = image.width * scale;
+      this.height = image.height * scale;
+
+      this.position = {
+        x: position.x,
+        y: position.y,
+      };
+    };
+  }
+
+  draw() {
+    // c.fillStyle = "red";
+    // c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    c.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+  }
+
+  update({ velocity }) {
+    if (this.image) {
+      this.draw();
+      this.position.x += velocity.x;
+      this.position.y += velocity.y;
+    }
+  }
+}
+
+class Projectile {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+
+    this.radius = 3;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = "red";
+    c.fill();
+    c.closePath();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
+class Grid {
+  constructor() {
+    this.position = {
+      x: 0,
+      y: 0,
+    };
+    this.velocity = {
+      x: 3,
+      y: 0,
+    };
+    this.invaders = [];
+
+    const columns = Math.floor(Math.random() * 10 + 5);
+    const rows = Math.floor(Math.random() * 5 + 2);
+
+    this.width = columns * 45;
+
+    for (let x = 0; x < columns; x++) {
+      for (let y = 0; y < rows; y++) {
+        this.invaders.push(
+          new Invader({
+            position: {
+              x: x * 45,
+              y: y * 35,
+            },
+          })
+        );
+      }
+    }
+  }
+
+  update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    this.velocity.y = 0;
+
+    if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
+      this.velocity.x = -this.velocity.x;
+      this.velocity.y = 30;
+    }
+  }
+}
+
 const player = new Player();
+const projectiles = [];
+const grids = [new Grid()];
+
 const keys = {
   a: {
     pressed: false,
@@ -74,13 +188,30 @@ const keys = {
   },
 };
 
-this.rotation = 0;
+let framse = 0;
 
 function animate() {
   requestAnimationFrame(animate);
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
+  projectiles.forEach((projectile, index) => {
+    if (projectile.position.y + projectile.radius <= 0) {
+      setTimeout(() => {
+        projectiles.splice(index, 1);
+      }),
+        0;
+    } else {
+      projectile.update();
+    }
+  });
+
+  grids.forEach((grid) => {
+    grid.update();
+    grid.invaders.forEach((invader) => {
+      invader.update({ velocity: grid.velocity });
+    });
+  });
 
   if (keys.a.pressed && player.position.x >= 0) {
     player.velocity.x = -6;
@@ -107,11 +238,20 @@ addEventListener("keydown", ({ key }) => {
     case "d":
       keys.d.pressed = true;
       break;
-    case "s":
-      console.log("down");
-      break;
-    case "w":
-      console.log("up");
+    case " ":
+      projectiles.push(
+        new Projectile({
+          position: {
+            x: player.position.x + player.width / 1.5,
+            y: player.position.y,
+          },
+          velocity: {
+            x: 0,
+            y: -11,
+          },
+        })
+      );
+      console.log(projectiles);
       break;
   }
 });
@@ -124,11 +264,7 @@ addEventListener("keyup", ({ key }) => {
     case "d":
       keys.d.pressed = false;
       break;
-    case "s":
-      console.log("down");
-      break;
-    case "w":
-      console.log("up");
+    case " ":
       break;
   }
 });
